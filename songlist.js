354eae1fbf5fab6dbaedc99cb671a28c25a689ca
Ryan Tanay <ryantanay@newevolution.org>
2015-07-27 18:53:15 -0500

    Removed commented out JS from earlier exercise, can always get it back via git if needed.

--- songlist.js
+++ songlist.js
@@ -27,32 +27,5 @@ $("#more").click(function(e) {
 
 $(".delete").click(function(e) {
   console.log("Ding");
-  this.parent().remove();
+  parent(this).remove();
 });
-
-// var contentDiv = document.getElementById("content");
-
-// var songs = [];
-
-// songs.push("Legs > by Z*ZTop on the album Eliminator",
-//   "The Logical Song > by Supertr@amp on the album Breakfast in America",
-//   "Another Brick in the Wall > by Pink Floyd on the album The Wall",
-//   "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction",
-//   "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill");
-
-// // add a song to the beginning
-// songs.unshift("Big Long Now > by Nirvana on the album Incesticide");
-// // add a song to the end
-// songs.push("The Notion of Backwards Motion > by Robot Science on the album Square");
-
-// for (var i=0; i<songs.length; i++) {
-//   var itemAsString = songs[i];
-//   itemAsString = itemAsString.replace(/\>/g, "-");
-//   itemAsString = itemAsString.replace(/\*/g, "");
-//   itemAsString = itemAsString.replace(/\@/g, "");
-//   itemAsString = itemAsString.replace(/\(/g, "");
-//   itemAsString = itemAsString.replace(/\!/g, "");
-//   songs[i] = "<p>" + itemAsString + "</p>";
-// }
-
-// contentDiv.innerHTML = songs.join("");
