cd3f37f63819a0130d2b9b4c16f481ebb2293e33
Ryan Tanay <ryantanay@newevolution.org>
2015-07-27 11:42:10 -0500

    Wrote out the JS to step through the received object's first value's array and pull out the relevant info into the proper HTML to display as per the original layout.

--- songlist.js
+++ songlist.js
@@ -1,26 +1,41 @@
-var contentDiv = document.getElementById("content");
+$(document).ready(function() {
+  $.ajax({
+    url: "songlist.json"
+  }).done(function(data) {
+    var songArr = data.songs;
+    for(var i=0; i<songArr.length; i++) {
+      $("#content").append("<section><h1>" + songArr[i].title + "</h1><ul>");
+      $("#content").append("<li>" + songArr[i].artist + "</li><li><span></span></li>");
+      $("#content").append("<li>" + songArr[i].album + "</li><li><span></span></li>");
+      $("#content").append("<li>" + songArr[i].genre + "</li></ul></section>");
+    }
+  })
+});
 
-var songs = [];
 
-songs.push("Legs > by Z*ZTop on the album Eliminator",
-  "The Logical Song > by Supertr@amp on the album Breakfast in America",
-  "Another Brick in the Wall > by Pink Floyd on the album The Wall",
-  "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction",
-  "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill");
+// var contentDiv = document.getElementById("content");
 
-// add a song to the beginning
-songs.unshift("Big Long Now > by Nirvana on the album Incesticide");
-// add a song to the end
-songs.push("The Notion of Backwards Motion > by Robot Science on the album Square");
+// var songs = [];
 
-for (var i=0; i<songs.length; i++) {
-  var itemAsString = songs[i];
-  itemAsString = itemAsString.replace(/\>/g, "-");
-  itemAsString = itemAsString.replace(/\*/g, "");
-  itemAsString = itemAsString.replace(/\@/g, "");
-  itemAsString = itemAsString.replace(/\(/g, "");
-  itemAsString = itemAsString.replace(/\!/g, "");
-  songs[i] = "<p>" + itemAsString + "</p>";
-}
+// songs.push("Legs > by Z*ZTop on the album Eliminator",
+//   "The Logical Song > by Supertr@amp on the album Breakfast in America",
+//   "Another Brick in the Wall > by Pink Floyd on the album The Wall",
+//   "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction",
+//   "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill");
 
-contentDiv.innerHTML = songs.join("");
+// // add a song to the beginning
+// songs.unshift("Big Long Now > by Nirvana on the album Incesticide");
+// // add a song to the end
+// songs.push("The Notion of Backwards Motion > by Robot Science on the album Square");
+
+// for (var i=0; i<songs.length; i++) {
+//   var itemAsString = songs[i];
+//   itemAsString = itemAsString.replace(/\>/g, "-");
+//   itemAsString = itemAsString.replace(/\*/g, "");
+//   itemAsString = itemAsString.replace(/\@/g, "");
+//   itemAsString = itemAsString.replace(/\(/g, "");
+//   itemAsString = itemAsString.replace(/\!/g, "");
+//   songs[i] = "<p>" + itemAsString + "</p>";
+// }
+
+// contentDiv.innerHTML = songs.join("");
