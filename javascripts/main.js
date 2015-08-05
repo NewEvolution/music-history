500a8234bfe4f95874543f2ca1e6b06eaa5e4fa0
Ryan Tanay <ryantanay@newevolution.org>
2015-08-05 09:13:44 -0500

    Converted the checkboxes to text area addons with disabled input to provide formatting.

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,7 +8,7 @@ requirejs(["dom-access", "populate-songs", "get-more-songs"], function(dom, popu
 
 function songWriter(dataArr, domEle) {
   for(var i=0; i<dataArr.length; i++) {
-    domEle.append("<section class='row'><div class='col-xs-1'><button class='delete btn btn-danger btn-xs'>X</button></div><div class='col-xs-11'><h1>" + 
+    domEle.append("<section class='row'><div class='col-xs-1'><button class='delete btn btn-danger btn-xs'><span class='glyphicon glyphicon-remove' aria-hidden='true'></span></button></div><div class='col-xs-11'><h1>" + 
       dataArr[i].title + "</h1><ul class='list-inline'><li>" + dataArr[i].artist + 
       "</li><li><span class='li-divider'></span></li><li>" + dataArr[i].album + 
       "</li><li><span class='li-divider'></span></li><li>" + dataArr[i].genre + 
