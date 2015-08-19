91334b09b8ac3a93946b003f1b03db081bb11738
Ryan Tanay <ryantanay@newevolution.org>
2015-08-19 14:51:04 -0500

    Pulled the last bit of functional code out of main.js into its own function.

--- javascripts/main.js
+++ javascripts/main.js
@@ -18,26 +18,6 @@ requirejs.config({
 });
 
 // The main function requiring all our anciliary scripts
-requirejs(["dependencies", "firebase", "q", "populate-songs", "song-list-handlers", "filter-handlers", "add-handlers"],
-function(dependencies, _firebase, Q, populate, slh, fh, ah){
-
-  var firebaseRef = new Firebase("https://sizzling-torch-4887.firebaseio.com/");
-  var currentPage = location.pathname.substring(1); // get the current HTML page name
-  var deferred = Q.defer();
-  
-  // Execute on DB change
-  firebaseRef.child("songs").on("value", function(snapshot) {
-    var retrievedSongsObj = snapshot.val();
-    populate.populatePage(retrievedSongsObj, currentPage, deferred);
-  });
-
-  // Song List Handlers
-  slh.handlers(firebaseRef);
-
-  deferred.promise.then(function(promisedObj) {
-    // Filter Form Handlers
-    fh.handlers(promisedObj.u_artists, promisedObj.u_albums, promisedObj.songs_a);
-    // Add Form Handlers
-    ah.handlers(promisedObj.u_artists, promisedObj.u_albums, promisedObj.songs_a, firebaseRef);
-  });
+requirejs(["dependencies", "scaffold"],
+function(dependencies, scaffold){
 });
---
+++ javascripts/scaffold.js
@@ -0,0 +1,28 @@
+define(function(require){
+  var populate = require("populate-songs");
+  var slh = require("song-list-handlers");
+  var _firebase = require("firebase");
+  var fh = require("filter-handlers");
+  var ah = require("add-handlers");
+  var Q = require("q");
+
+  var firebaseRef = new Firebase("https://sizzling-torch-4887.firebaseio.com/");
+  var currentPage = location.pathname.substring(1); // get the current HTML page name
+  var deferred = Q.defer();
+  
+  // Execute on DB change
+  firebaseRef.child("songs").on("value", function(snapshot) {
+    var retrievedSongsObj = snapshot.val();
+    populate.populatePage(retrievedSongsObj, currentPage, deferred);
+  });
+
+  // Song List Handlers
+  slh.handlers(firebaseRef);
+
+  deferred.promise.then(function(promisedObj) {
+    // Filter Form Handlers
+    fh.handlers(promisedObj.u_artists, promisedObj.u_albums, promisedObj.songs_a);
+    // Add Form Handlers
+    ah.handlers(promisedObj.u_artists, promisedObj.u_albums, promisedObj.songs_a, firebaseRef);
+  });
+});
