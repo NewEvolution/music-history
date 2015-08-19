d99a448d7d98f21e26e9946d9e32e394c4a646ee
Ryan Tanay <ryantanay@newevolution.org>
2015-08-19 13:42:06 -0500

    Swapped out single quotes for double quotes.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,18 +1,18 @@
 // Pull in all the various javascript libraries
 requirejs.config({
-  baseUrl: './javascripts',
+  baseUrl: "./javascripts",
   paths: {
-    'jquery': '../lib/bower_components/jquery/dist/jquery.min',
-    'firebase': '../lib/bower_components/firebase/firebase',
-    'lodash': '../lib/bower_components/lodash/lodash.min',
-    'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
-    'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min',
-    'q': '../lib/bower_components/q/q'
+    "jquery": "../lib/bower_components/jquery/dist/jquery.min",
+    "firebase": "../lib/bower_components/firebase/firebase",
+    "lodash": "../lib/bower_components/lodash/lodash.min",
+    "hbs": "../lib/bower_components/require-handlebars-plugin/hbs",
+    "bootstrap": "../lib/bower_components/bootstrap/dist/js/bootstrap.min",
+    "q": "../lib/bower_components/q/q"
   },
   shim: {
-    'bootstrap': ['jquery'],
-    'firebase': {
-      exports: 'Firebase'
+    "bootstrap": ["jquery"],
+    "firebase": {
+      exports: "Firebase"
     }
   }
 });
