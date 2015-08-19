4cf28b8728d305c15979bcc8a56efd6ab8145694
Ryan Tanay <ryantanay@newevolution.org>
2015-08-19 12:01:11 -0500

    Fixed relative paths for requirejs in new location

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,12 +2,12 @@
 requirejs.config({
   baseUrl: './javascripts',
   paths: {
-    'jquery': '../bower_components/jquery/dist/jquery.min',
-    'firebase': '../bower_components/firebase/firebase',
-    'lodash': '../bower_components/lodash/lodash.min',
-    'hbs': '../bower_components/require-handlebars-plugin/hbs',
-    'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min',
-    'q': '../bower_components/q/q'
+    'jquery': '../lib/bower_components/jquery/dist/jquery.min',
+    'firebase': '../lib/bower_components/firebase/firebase',
+    'lodash': '../lib/bower_components/lodash/lodash.min',
+    'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
+    'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min',
+    'q': '../lib/bower_components/q/q'
   },
   shim: {
     'bootstrap': ['jquery'],
