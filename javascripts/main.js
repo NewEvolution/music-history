c22a8ae0f7eb7effef86674493db885ee610b0f0
Ryan Tanay <ryantanay@newevolution.org>
2015-08-19 11:23:11 -0500

    Removed extraneous require modules from main.js.

--- javascripts/main.js
+++ javascripts/main.js
@@ -18,25 +18,9 @@ requirejs.config({
 });
 
 // The main function requiring all our anciliary scripts
-requirejs(["jquery", "lodash", "firebase", "hbs", "bootstrap", "q", "add-songs", "filter-songs",
-  "populate-songs", "multiuse-functions", "song-list-handlers", "filter-handlers", "add-handlers", "hb-template"], 
-function($, _, _firebase, Handlebars, bootstrap, Q, addSongs, filterSongs, populate, muf, slh, fh, ah, template){
+requirejs(["firebase", "hbs", "bootstrap", "q", "populate-songs", "song-list-handlers", "filter-handlers", "add-handlers"],
+function(_firebase, Handlebars, bootstrap, Q, populate, slh, fh, ah){
 
-// requirejs(function(require) {
-//   var $ = require("jquery");
-//   var _ = require("lodash");
-//   var _firebase = require("firebase");
-//   var Handlebars = require("hbs");
-//   var bootstrap = require("bootstrap");
-//   var Q = require("q");
-//   var addSongs = require("add-songs");
-//   var filterSongs = require("filter-songs");
-//   var populate = require("populate-songs");
-//   var muf = require("multiuse-functions");
-//   var slh = require("song-list-handlers");
-//   var fh = require("filter-handlers");
-//   var ah = require("add-handlers");
-//   var template = require("hb-template");
   var firebaseRef = new Firebase("https://sizzling-torch-4887.firebaseio.com/");
   var currentPage = location.pathname.substring(1); // get the current HTML page name
   var deferred = Q.defer();
