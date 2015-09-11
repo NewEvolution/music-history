ad7da37ef6b1675b88bb758b775965c800da2ea5
Ryan Tanay <ryantanay@newevolution.org>
2015-09-11 14:38:15 -0500

    Discovered the reason I was having to add in jquery to the dependencies module was that I had left the shim portion of the requirejs task off in the Gruntfile.  Rectified, removed from dependencies, recompiled and ready to rock.

--- converted/dependencies.js
+++ converted/dependencies.js
@@ -1,4 +1,4 @@
-define(["exports", "jquery", "hbs", "firebase", "bootstrap"], function (exports, _jquery, _hbs, _firebase, _bootstrap) {
+define(["exports", "hbs", "firebase", "bootstrap"], function (exports, _hbs, _firebase, _bootstrap) {
   "use strict";
 });
 //# sourceMappingURL=dependencies.js.map
--- javascripts/dependencies.js
+++ javascripts/dependencies.js
@@ -1,4 +1,3 @@
-import * as $ from "jquery";
 import * as Handlebars from "hbs";
 import * as firebase from "firebase";
 import * as bootstrap from "bootstrap";
